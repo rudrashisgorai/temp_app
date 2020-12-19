faac6445884ec52b83e1049539ee0e3e3d6f2ff2
Rudrashis Gorai <53018046+rudrashisgorai@users.noreply.github.com>
2020-12-20 01:16:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import  Flask,request
+import requests
+import json
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def temp():
+    key = '5eb8acfd55b37163529efa9cbd1acbfb'
+    try:
+        city = request.args.get('city', type =str)
+        call = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}"
+        data = requests.get(call)
+        city_data = data.json()
+        return f"<h1>Temperature in {city_data['name']} is {round(city_data['main']['temp']-273)}</h1>"
+    except:
+        return "<h1>try again</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
